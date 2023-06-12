df44494a2a1425b0c295783e3f19ffb68e86cce6
Denise Capitaneo <decapitaneo@gmail.com>
2023-06-12 10:47:56 -0400

    aula 14 finalizada

--- Aula14--RequisicoesAssincronasNoReact/src/App.jsx
+++ Aula14--RequisicoesAssincronasNoReact/src/App.jsx
@@ -6,21 +6,55 @@ import { useEffect, useState } from 'react';
 function App() {
   const [estados, setEstados] = useState([]);
   const [cidades, setCidades] = useState([]);
-  
   const [estadoSelecionado, setEstadoSelecionado] = useState();
+  
+  const [cidadeSelecionada, setCidadeSelecionada] = useState();
   
   useEffect(
     () => {
-      buscaEstadosApi();  
+      buscandoEstadosApi();  
     },[]
   );
   
+  const buscandoEstadosApi = async () => {
+    console.log("Buscando Estados Brasileiros");
+
+    const response = await axios.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados");
+    setEstados([
+      {
+        id: 0,
+        sigla: "Selecione..."
+      },
+      ...response.data
+    ]);
+    console.log(response.data);
+  }
+
+  useEffect(
+    () => {
+      if (estadoSelecionado) {
+        buscandoCidadesApi();
+      }
+    }, [estadoSelecionado]
+  );
+
   
-  const buscaCidadesApi = async () => {
-    console.log("Buscando cidades brasieliras")
-    const response = await axios.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoSelecionado}/municipios")
+  const buscandoCidadesApi = async () => {
+    console.log("Buscando Cidades/Municipios Brasileiros");
+
+    const response = await axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoSelecionado}/municipios`);
+    setCidades([
+      {
+        id: 0,
+        nome: "Selecione..."
+      },
+      ...response.data
+    ]);
+    console.log(response.data);
   }
   
+
+  /*
   const buscaEstadosApi = async () =>{
       console.log("Buscando estads brasileiros....");
       const response = await axios.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados");
@@ -36,7 +70,7 @@ function App() {
       console.log(response.data);
   }
   
-  
+  */
   /* Axios está instalado no projeto! 
     -> https://www.npmjs.com/package/axios
   */
@@ -50,64 +84,65 @@ function App() {
     https://servicodados.ibge.gov.br/api/v1/localidades/estados/{UF}/municipios
  */
 
-  return (
-    <>
-
-      <div className='div-selects'>
-
-        <h1>IBGE: Localidades</h1>
-
-        <label>Selecione o estado</label>
-        <br />
-        <select
-          value={estadoSelecionado}
-          onChange={(e) => setEstadoSelecionado(e.target.value)}
-        >
-          {///populando o select
-            estados.map(estado => {
-              return(
-                <option
-                  key={estado.id}
-                  value={estado.sigla}>
-                  {estado.sigla} - {estado.nome}
-                </option>
-              
-              )
-            })
-          }
-          <option value="default">Selecione...</option>
-          <option value="RS">RS</option>
-          <option value="SP">SP</option>
-          <option value="RJ">RJ</option>
-        </select>
-
-        <br />
-        <br />
-
-        <label>Selecione a cidade</label>
-        <br />
-        <select
-          disabled={true}
-          value={"city1"}
-          onChange={() => { }}
-        >
-          <option value="default">Selecione...</option>
-          <option value="city1">Cidade 1</option>
-          <option value="city2">Cidade 2</option>
-          <option value="city3">Cidade 3</option>
-        </select>
-
-
-      </div>
-
-
-      <div className='div-resposta'>
-        <h3>Dados selecionados</h3>
-        <p>Estado: {estadoSelecionado ?? "Indefinido"}</p>
-        <p>Cidade: {"Cidade 1" ?? "Indefinida"}</p>
-      </div>
-    </>
-  )
-}
+    return (
+      <>
+  
+        <div className='div-selects'>
+  
+          <h1>IBGE: Localidades</h1>
+  
+          <label>Selecione o estado</label>
+          <br />
+          <select
+            value={estadoSelecionado}
+            onChange={(e) => setEstadoSelecionado(e.target.value)}
+          >
+            {
+              estados.map(estado => {
+                return (
+                  <option
+                    key={estado.id}
+                    value={estado.sigla}
+                  >{estado.sigla} - {estado.nome}</option>
+                )
+              })
+            }
+          </select>
+  
+          <br />
+          <br />
+  
+          <label>Selecione a cidade</label>
+          <br />
+          <select
+            disabled={cidades.length > 0 ? false : true}
+            value={cidadeSelecionada}
+            onChange={(e) => { setECidadeSelecionado(e.target.value) }}
+          >
+            {
+              cidades.map(cidade => {
+                return (
+                  <option
+                    key={cidade.id}
+                    value={cidade.nome}>
+                    {cidade.nome}
+                  </option>
+                )
+              })
+            }
+          </select>
+  
+  
+        </div>
+  
+  
+        <div className='div-resposta'>
+          <h3>Dados selecionados</h3>
+          <p>Estado: {estadoSelecionado ?? "Indefinido"}</p>
+          <p>Cidade: {cidadeSelecionada ?? "Indefinida"}</p>
+        </div>
+      </>
+    )
+  }
 
 export default App;
